5fd89ca31b495009f7cca9a7fd7b0b023fa2be47
NikNosakin <NikNosakin@yandex.ru>
2026-04-02 16:21:51 +0300

    Add test

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,4 +1,21 @@
 from src.calculator import add
+import pytest
+import sys
 
 def test_add():
     assert add(2, 3) == 5
+
+
+@pytest.mark.smoke
+def test_pop_from_empty_list():
+    with pytest.raises(IndexError):
+        lst=[]
+        assert not lst.pop()
+
+@pytest.mark.skipif(sys.version_info < (3, 10), reason="Requires Python 3.10+")
+def test_python310_feature():
+    # Тест использует match-case, который появился в 3.10
+    value = 1
+    match value:
+        case 1:
+            assert True
